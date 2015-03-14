1108f57604385698a64dc7764637d3a6ef991c75
Apollo <jango00@hotmail.de>
2015-03-15 00:33:01 +0100

    little change

--- Viktor/Viktor/Spells.cs
+++ Viktor/Viktor/Spells.cs
@@ -17,7 +17,7 @@ namespace Viktor
         public static void Init()
         {
             Spell[SpellSlot.Q].SetTargetted(0.25f, 2000);
-            Spell[SpellSlot.W].SetSkillshot(1.5f, 300, float.MaxValue, false, SkillshotType.SkillshotCircle);
+            Spell[SpellSlot.W].SetSkillshot(0.25f, 300, float.MaxValue, false, SkillshotType.SkillshotCircle);
             Spell[SpellSlot.E].SetSkillshot(0.0f, 90, 1000, false, SkillshotType.SkillshotLine);
             Spell[SpellSlot.R].SetSkillshot(0.25f, 250, float.MaxValue, false, SkillshotType.SkillshotCircle);
         }
